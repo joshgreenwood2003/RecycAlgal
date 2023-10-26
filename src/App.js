176db412f341c23a2ea143872a1d0b2deb98e578
Josh Greenwood <joshgreenwood2003@gmail.com>
2023-10-26 16:31:13 +0100

    initial details

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 
 import './App.css';
 import ResponsiveAppBar from './navbar';
-import { Box } from '@mui/material';
 import background from './algae.png'
 function App() {
 
-  const styles = {
-    paperContainer: {
-        height: 1356,
-        backgroundImage: `url(${"algae.png"})`
-    }
-  };
-
   return (
-    <div className="App" style = {{backgroundImage:"./algae.png"}}>
-
-
+    <div className="App" >
+      <div  style={{backgroundImage: `url(${background})`,backgroundSize: "cover",height: "100vh",color: "#f5f5f5"}}>
       <ResponsiveAppBar></ResponsiveAppBar>
-      <header   style={{
-    backgroundImage: `url(${background})`,
-    backgroundSize: "cover",
-   // height: "100vh",
-    color: "#f5f5f5"
-}} className="App-header">
-        <p>
-          Brought to you by our wonderful researcher Joelle Harrison
-        </p>
-      </header>
+      <h1>About us</h1>
+      <p>We are a new company proposing a novel way to decompose polymers</p>
+      </div>
     </div>
   );
 }
