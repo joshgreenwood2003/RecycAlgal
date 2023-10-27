949500c24d9284521922f28f48e2fba831d4392b
Josh Greenwood <joshgreenwood2003@gmail.com>
2023-10-27 10:38:55 +0100

    turtle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import ResponsiveAppBar from './navbar';
 import background from './algae.png'
+import turtle from './turtle.jpg'
 import Paper from '@mui/material/Paper';
 function App() {
 
@@ -11,13 +12,9 @@ function App() {
       <ResponsiveAppBar></ResponsiveAppBar>
       <Paper elevation = {5}style={{backgroundColor:"rgba(0, 0, 0, 0.8)",maxWidth:"50%",margin:"auto"}}>
       <h1 style={{opacity:"1"}}>About us</h1>
-      <p style = {{opacity:"1"}}>We are a new company proposing a novel way to decompose polymers</p>
+      <p style = {{opacity:"1"}}>We are a new company proposing a novel way to decompose plastic. Through our production of plastic-degrading bioengineered microalgae, we aim to develop a more sustainable alternative to incineration, landfill and recycling</p>
+     <img height="500px" src={turtle}></img>
       <p>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
         <br/>
         <br/>
         <br/><br/><br/><br/><br/><br/>
